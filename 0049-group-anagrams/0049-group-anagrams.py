a881cd707cd824dedeb520caa1e2de263869c41a
slalani13 <79872105+slalani13@users.noreply.github.com>
2024-04-03 20:42:28 -0500

    Time: 99 ms (24.68%), Space: 22.2 MB (20.09%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,22 +1,18 @@
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # treat anagram as a list of letters with a count
-        
-
-        result = defaultdict(list)
+        maps = dict()
 
         for word in strs:
-            count = [0] * 26 # for letters of the alphabet, this is the key
-            # at the end of loop, count is updated for the word
+            # each letter will be an index
+            wordList = [0]*26
             for l in word:
-                # gets ascii value to find index of letter
-                count[ord(l) - ord('a')] += 1 
-            key = tuple(count)
-            # appending value with same key, or adding new key
-            result[key].append(word)
-            # print(result)
+                index=ord(l) - ord('a')
+                wordList[index] +=1
+            wordTuple = tuple(wordList)
+            if wordTuple in maps:
+                maps[wordTuple].append(word)
+            else:
+                maps[wordTuple] = [word]
         
-        return result.values()
-
+        return maps.values()
         
