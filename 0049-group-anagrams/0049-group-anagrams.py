0ec74f5dc162a032189b88dc41359d3e07bb4bd0
slalani13 <79872105+slalani13@users.noreply.github.com>
2024-04-03 20:48:56 -0500

    Time: 102 ms (19.18%), Space: 22.3 MB (18.64%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,6 +1,6 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        maps = dict()
+        maps = defaultdict(list)
 
         for word in strs:
             # each letter will be an index
@@ -8,11 +8,12 @@ class Solution:
             for l in word:
                 index=ord(l) - ord('a')
                 wordList[index] +=1
-            wordTuple = tuple(wordList)
-            if wordTuple in maps:
-                maps[wordTuple].append(word)
-            else:
-                maps[wordTuple] = [word]
+            # wordTuple = tuple(wordList)
+            maps[tuple(wordList)].append(word)
+            # if wordTuple in maps:
+            #     maps[wordTuple].append(word)
+            # else:
+            #     maps[wordTuple] = [word]
         
         return maps.values()
         
