ed373ae8ab42ecc02efb442c31b4f1453d4b7395
slalani13 <79872105+slalani13@users.noreply.github.com>
2024-04-22 12:01:06 -0500

    Time: 42 ms (87.41%), Space: 17 MB (64.7%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,18 +1,19 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_map = {}
-        t_map = {}
-        for l in s:
-            if l in s_map:
-                s_map[l] += 1
-            else:
-                s_map[l] = 1
-        for l in t:
-            if l in t_map:
-                t_map[l] += 1
-            else:
-                t_map[l] = 1
-        if s_map == t_map:
+        #edge cases
+        if len(s) != len(t):
+            return False
+        if len(s) == 0:
+            return True
+        strList = [0]*26
+        strList2 = [0]*26
+        print(strList)
+        for char in s:
+            index = ord(char) - ord('a')
+            strList[index] += 1
+        for char in t:
+            index = ord(char) - ord('a')
+            strList2[index] += 1
+        if strList == strList2:
             return True
         return False
-        
