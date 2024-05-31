92fa7aaee041b3ec0cc20e076ebc64d0e8ff91b3
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2024-05-31 12:56:07 -0500

    Time: 358 ms (17.13%), Space: 16.4 MB (97.6%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,7 +1,8 @@
 class Solution:
     def fib(self, n: int) -> int:
-        #calculate base case
-        if n <= 1:
-            return n
-
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        
         return self.fib(n-1) + self.fib(n-2)
