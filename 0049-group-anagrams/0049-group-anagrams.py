8e776fec8e144811dade0e3154516f7600309858
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2024-07-07 20:44:39 -0500

    Time: 86 ms (73.67%), Space: 22.2 MB (22.47%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,19 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        maps = defaultdict(list)
-
-        for word in strs:
-            # each letter will be an index
-            wordList = [0]*26
-            for l in word:
-                index=ord(l) - ord('a')
-                wordList[index] +=1
-            # wordTuple = tuple(wordList)
-            maps[tuple(wordList)].append(word)
-            # if wordTuple in maps:
-            #     maps[wordTuple].append(word)
-            # else:
-            #     maps[wordTuple] = [word]
-        
-        return maps.values()
-        
+        if len(strs) == 1:
+            return [strs]
+        # map key: tuple, val: list of strings
+        m = dict()
+        for s in strs:
+            l = [0]*26
+            for c in s:
+                index = ord(c) - ord('a')
+                l[index] += 1
+            key = tuple(l)
+            if key in m:
+                m[key].append(s)
+            else:
+                m[key] = [s]
+        return m.values()
