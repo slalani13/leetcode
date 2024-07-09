311d41e9e32682c30191c6d710265a0ef4853570
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2024-07-09 17:44:02 -0500

    Time: 185 ms (84.45%), Space: 18 MB (64.12%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,19 +1,15 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        #edge cases
-        if len(nums) == 0:
-            return -1
-        L = 0
-        R = len(nums)-1
-        mid = (L+R)//2
-        print(mid)
-        while L <= R:
-            if target < nums[mid]:
-                R=mid-1
-                mid = (L+R)//2
-            elif target > nums[mid]:
-                L=mid+1
-                mid = (L+R)//2
+        l, r = 0, len(nums)-1
+        while l <= r:
+            m = l + (r-1//2)
+            if nums[m] > target:
+                r = m-1
+            elif nums[m] < target:
+                l = m+1
             else:
-                return mid
+                return m
         return -1
+
+        
+
