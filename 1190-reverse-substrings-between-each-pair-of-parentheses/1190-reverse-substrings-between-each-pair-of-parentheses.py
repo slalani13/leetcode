b54e07efc243d1855b0004801be50925564680f3
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2024-07-11 00:19:00 -0500

    Time: 35 ms (61.58%), Space: 16.6 MB (40.89%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.py
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.py
@@ -8,19 +8,12 @@ class Solution:
         for c in s:
             if c != ')': # [(ulove
                 stack.append(c) # 
-                # print(stack)
             else:
                 reversed_word = "" #
-                #print(stack[-1]) #e, v, o, l, i
                 while stack[-1] != '(':
                     reversed_word += stack[-1] #evol
-                    # print("r", reversed_word)
                     stack.pop()
-                    print("v", stack[-1])
-                # print("reversedWord: ", reversed_word) 
                 stack.pop()
-                # print("current stack", stack)
                 for w in reversed_word:
                     stack.append(w)
-                # print("new_stack", stack)
         return "".join(stack)
