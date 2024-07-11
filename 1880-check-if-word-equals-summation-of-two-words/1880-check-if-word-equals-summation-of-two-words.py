adc5187d2c5ecb6c30bc51785417df295b0c1b1c
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2024-07-10 23:33:50 -0500

    Time: 42 ms (11.13%), Space: 16.5 MB (60.7%) - LeetHub

--- 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.py
+++ 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.py
@@ -1,53 +1,24 @@
 class Solution:
     def isSumEqual(self, firstWord: str, secondWord: str, targetWord: str) -> bool:
-        #acb + cba = 021 + 210  = 231 = cdb
-
-        # change the length of the smaller word so they are equal
-        while len(firstWord) != len(secondWord):
-            if len(firstWord) < len(secondWord):
-                firstWord = 'a' + firstWord
-            else:
-                secondWord = 'a' + secondWord
-        # add them together
-        sum_array = []
-        for i in range(len(firstWord)):
-            # 2-1 = 1
-            num1 = ord(firstWord[i])-ord('a') # 0,2,1
-            # print("num1: " + str(num1))
-            num2 = ord(secondWord[i])-ord('a') # 2,1,0
-            # print("num2: "+ str(num2))
-            sum = num1 + num2
-            sum_array.append(sum)
-        # [2,3,1]
-        print(sum_array)
-        # sum_array[3,2,1] = 300 + 20 + 1
-        final_sum = 0
-        exp = len(sum_array) - 1
-        for num in sum_array:
-            # print(exp)
-            real_num = num * 10**exp
-            final_sum += real_num
-            # print(final_sum)
-            exp-=1
-            
-        
-        print(final_sum)
-        # convert our target word into an interger and compare
-        exp = len(targetWord)-1
-        target_arr = []
-        for l in targetWord:
-            print(l)
-            number = ord(l) - ord('a')
-            target_arr.append(number)
-        
-        target_sum = 0
-        for num in target_arr:
-            real_num = num * 10**exp
-            target_sum += real_num
-            exp-=1
-        
-        if target_sum == final_sum:
+        l1 = len(firstWord)
+        l2 = len(secondWord)
+        l3 = len(targetWord)
+        firstWordInt = 0
+        secondWordInt = 0
+        targetWordInt = 0
+        for i in range(l1):
+            number = ord(firstWord[i]) - ord('a')
+            number = number * 10**(l1-i+1)
+            firstWordInt += number
+        for i in range(l2):
+            number = ord(secondWord[i]) - ord('a')
+            number = number * 10**(l2-i+1)
+            secondWordInt += number
+        for i in range(l3):
+            number = ord(targetWord[i]) - ord('a')
+            number = number * 10**(l3-i+1)
+            targetWordInt += number
+        if targetWordInt == firstWordInt + secondWordInt:
             return True
-        else:
-            return False
-
+        return False
+        
