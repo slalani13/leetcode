a6e421a6107dbbcfb06d8b81c773c7bbc0673824
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2024-08-12 12:08:03 -0500

    Time: 49 ms (95.64%), Space: 17.7 MB (52.15%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        m = dict()
-        for i, num in enumerate(nums):
-            m[num] = i
-        print(m)
+        hashMap = dict()
         for i, n in enumerate(nums):
-            desired = target - n
-            if desired in m and m[desired] != i:
-                return m[desired], i
+            key, value = n, i
+            hashMap[key] = value
+        for i, n in enumerate(nums):
+            goal = target - n
+            if goal in hashMap and hashMap[goal] != i:
+                return i, hashMap[goal]
         
