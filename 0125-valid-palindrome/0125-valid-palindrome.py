3dbdae09da059a5a8f9ec5d68855a6bfcd682f3d
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2024-08-12 16:12:32 -0500

    Time: 55 ms (15.28%), Space: 16.9 MB (80.82%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,12 +1,13 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        sList = [val.lower() for val in s if val.isalnum()]
-        string1 = "".join(sList)
-        print(string1)
-        reverseString = ""
-        for i in range(len(string1)-1, -1, -1):
-            reverseString += string1[i]
-        print(reverseString)
-        if string1 == reverseString:
-            return True
-        return False
+        newS = ''
+        for c in s:
+            if c.isalnum():
+                newS += c.lower()
+        start, end = 0, len(newS)-1
+        while start <= end:
+            if newS[start] != newS[end]:
+                return False
+            start+= 1
+            end -= 1
+        return True
