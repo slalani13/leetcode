24deef6d65cb3daa167ac4d0dd7cb076c1e3d805
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2024-08-17 12:31:22 -0500

    Time: 32 ms (77.57%), Space: 16.6 MB (11.05%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,29 +1,21 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        mp = {"}":"{","]":"[",")":"("}
-        stack = []
-        if len(s) == 0:
-            return True
         if len(s) == 1:
             return False
-        
-        for c in s:
-            # opening bracket
-            if c not in mp:
-                stack.append(c)
-                continue
-            # closing bracket at beginning
-            elif not stack and c in mp:
+        hashmap = {")": "(", "]":"[", "}":"{"}
+        stack = []
+        for i in range(len(s)):
+            if not stack and s[i] in hashmap:
                 return False
-            # closing bracket == opening bracket
-            elif c in mp and stack[-1] == mp[c]:
-                stack.pop(-1)
-                continue
-            # none of the above
+            elif stack and s[i] in hashmap:
+                if stack[-1] != hashmap[s[i]]:
+                    return False
+                else:
+                    stack.pop()
             else:
-                return False
-        
+                stack.append(s[i])
         if not stack:
             return True
         else:
             return False
+                
