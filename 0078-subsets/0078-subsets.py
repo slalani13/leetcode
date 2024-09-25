6923f5cb9112874ed5859127deabf88936525118
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2024-09-25 08:49:02 -0500

    Time: 37 ms (55.01%), Space: 16.7 MB (25.77%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,18 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res, sol = [], []
-        
+
         def backtrack(i):
-            #base case
-            if i >= len(nums):
+            if i == len(nums):
                 res.append(sol[:])
                 return
-            # make a decision
+            # don't choose
             backtrack(i+1)
-
             sol.append(nums[i])
             backtrack(i+1)
             sol.pop()
-        
         backtrack(0)
         return res
