f061d75c24b9890408f5d64962a8a9e0ba3300d2
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2024-09-30 17:13:52 -0400

    Time: 37 ms (56.64%), Space: 17.8 MB (6.64%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,22 +5,13 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        #edge cases
-        if not head:
-            return None
-        if not head.next:
-            return head
-        
-        dummy = None
-        prev = dummy
-        curr = head
-        next1 = curr.next
+        prev, cur = None, head
 
-        while curr != None:
-            curr.next = prev
-            prev = curr
-            curr = next1
-            if next1:
-                next1 = curr.next
+        while cur:
+            tmp= cur.next
+            cur.next = prev
+            prev = cur
+            cur = tmp
         
         return prev
+            
