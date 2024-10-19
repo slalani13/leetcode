62b9b0eade2839dd49d50d610e6864f86caab2c4
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2024-10-18 22:50:54 -0500

    Time: 14 ms (100%), Space: 20.4 MB (47.36%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,20 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        if len(strs) <= 1:
-            return [strs]
-        hashMap = {}
-        res = []
-        print(hashMap)
+        hashmap = defaultdict(list)
         for word in strs:
-            wordCount = [0]*26
-            for c in word:
-                index = ord(c) - ord('a')
-                wordCount[index] += 1
-            key = tuple(wordCount)
-            if key in hashMap:
-                hashMap[key].append(word)
-            else:
-                hashMap[key] = [word]
-        for val in hashMap.values():
+            key = sorted(word)
+            key = tuple(key)
+            hashmap[key].append(word)
+        
+        res = []
+        for val in hashmap.values():
             res.append(val)
+
         return res
