ec09a6335231a0b0b7f46a31a289cbed267ad941
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2024-10-21 15:54:11 -0500

    Time: 462 ms (98.58%), Space: 19.8 MB (97.83%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,19 +1,25 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        numSort = sorted(nums)
-        s = set()
+        nums.sort()
+        # [-4, -1, -1, 0, 1, 2]
+        res = []
         for i in range(len(nums)):
-            j = i+1
-            k = len(nums)-1
-            if i != 0 and numSort[i] == numSort[i-1]:
+            if nums[i] > 0:
+                break
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
-            while j < k:
-                if numSort[i] + numSort[j] + numSort[k] == 0:
-                    s.add(tuple([numSort[i], numSort[j], numSort[k]]))
-                    j += 1
-                    continue
-                elif numSort[i] + numSort[j] + numSort[k] < 0:
-                    j += 1
+            lo, hi = i+1, len(nums)-1
+            while lo < hi:
+                summ = nums[i] + nums[lo] + nums[hi]
+                if summ == 0:
+                    res.append([nums[i], nums[lo], nums[hi]])
+                    lo, hi = lo + 1, hi -1
+                    while lo < hi and nums[lo] == nums[lo-1]:
+                        lo += 1
+                    while lo < hi and nums[hi] == nums[hi+1]:
+                        hi -= 1
+                elif summ < 0:
+                    lo += 1
                 else:
-                    k -= 1
-        return s
+                    hi -= 1
+        return res
