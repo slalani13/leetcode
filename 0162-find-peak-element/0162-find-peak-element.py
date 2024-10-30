68669f3f56bd7a61f75740a50028c19cf03864d6
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2024-10-30 16:36:04 -0500

    Time: 0 ms (100%), Space: 16.9 MB (8.2%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -4,9 +4,9 @@ class Solution:
         L, R = 0, n-1
 
         while L < R:
-            mid = L + (R-L+1) // 2
+            mid = L + (R-L) // 2
             if nums[mid] > nums[mid+1]:
                 R = mid
             else:
                 L = mid + 1
-        return R
+        return L
