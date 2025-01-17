9fac62a5d5910b9382f8b7d9d2e5c0e0e82c2284
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-01-17 10:58:10 -0600

    Time: 11 ms (74.72%), Space: 18 MB (21.61%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,34 +1,34 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # if len(s) != len(t):
-        #     return False
+        if len(s) != len(t):
+            return False
 
-        # s1 = [0] * 26
-        # t1 = [0] * 26
+        s1 = [0] * 26
+        t1 = [0] * 26
 
-        # for i in range(len(s)):
-        #     s_idx = ord(s[i]) - ord('a')
-        #     t_idx = ord(t[i]) - ord('a')
-        #     s1[s_idx] += 1
-        #     t1[t_idx] += 1
-        
-        # if s1 != t1:
-        #     return False
-        # return True
-        
-        def rec(i, s1, t1):
-            if len(s) != len(t):
-                return False
-            if i == len(s):
-                if s1 != t1:
-                    return False
-                else:
-                    return True
-                
+        for i in range(len(s)):
             s_idx = ord(s[i]) - ord('a')
             t_idx = ord(t[i]) - ord('a')
             s1[s_idx] += 1
             t1[t_idx] += 1
-            return rec(i+1, s1, t1)
         
-        return rec(0, [0]*26, [0]*26)
+        if s1 != t1:
+            return False
+        return True
+        
+        # def rec(i, s1, t1):
+        #     if len(s) != len(t):
+        #         return False
+        #     if i == len(s):
+        #         if s1 != t1:
+        #             return False
+        #         else:
+        #             return True
+                
+        #     s_idx = ord(s[i]) - ord('a')
+        #     t_idx = ord(t[i]) - ord('a')
+        #     s1[s_idx] += 1
+        #     t1[t_idx] += 1
+        #     return rec(i+1, s1, t1)
+        
+        # return rec(0, [0]*26, [0]*26)
