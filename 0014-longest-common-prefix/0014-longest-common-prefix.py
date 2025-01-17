0ca338c70498d8319f5a60cbb9e453233b05b4af
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-01-17 12:44:59 -0600

    Time: 0 ms (100%), Space: 17.9 MB (25.65%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -4,12 +4,27 @@ class Solution:
         min_length = float("inf")
         for i in range(len(strs)):
             min_length = min(min_length, len(strs[i]))
-        for i in range(min_length):
+        # for i in range(min_length):
+        #     for j in range(len(strs)-1):
+        #         if strs[j][i] == strs[j+1][i]:
+        #             continue
+        #         else:
+        #             return prefix
+        #     prefix += strs[0][i]
+        # return prefix
+        
+        def rec(i, min_length, prefix):
+            # check at each recursive call
+            if (i > min_length-1):
+                return prefix
             for j in range(len(strs)-1):
                 if strs[j][i] == strs[j+1][i]:
                     continue
                 else:
                     return prefix
+            
             prefix += strs[0][i]
-        return prefix
+            return rec(i+1, min_length, prefix)
         
+        return rec(0, min_length, "")
+            
