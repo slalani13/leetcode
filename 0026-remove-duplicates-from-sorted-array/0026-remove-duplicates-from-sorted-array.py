4e085984840553d36deb4f83dfad75d1eb18d758
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-01-24 06:24:11 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,26 +1,28 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        # consider a sorted array in non-decreasing order
-        # remove the duplicates in place such that each unique element appears only once
-        # return the number of unique elements, k
+        # if len(nums) <= 1:
+        #     return len(nums)
 
-        # Cases:
-        # length of array is always at least 1
-        # [1, 1, 2] -> [1, 2, _]
-        # Base case: [1], [] -> if len(nums) <= 1: return len(nums)
-        # [1, 1, 1, 1, 1] -> [1, _]: k=1
-        # [1, 2]
-        if len(nums) <= 1:
-            return len(nums)
+        # L, R = 1, 1
+        # while R < len(nums):
+        #     if nums[R] != nums[R-1]:
+        #         nums[L] = nums[R]
+        #         L += 1
+        #     R += 1
+        # return L
 
-        L, R = 1, 1
-        while R < len(nums):
-            # what is the condition at which we stop iterating and process?
-            # when nums[R] does not equal its previous value, i.e new unique val
-            if nums[R] != nums[R-1]:
-                nums[L] = nums[R]
-                L += 1
-            R += 1
-        return L
+        # write this recursively
+        # [ 1, 2, 3], [1, 1, 1]
+        def rec(index, unique_index):
+            if index == len(nums):
+                return unique_index
+            
+            # case when index != index-1
+            if index == 0 or nums[index] != nums[index-1]:
+                nums[unique_index] == nums[index]
+                return rec(index+1, unique_index+1)
+
+            # case when index == index-1
+            return (index+1, unique_index)
         
 
