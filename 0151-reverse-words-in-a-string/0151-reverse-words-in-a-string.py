c6f5b7ff6b219e3b67db64b32d3f40b78b387639
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-01-30 20:56:56 -0600

    Time: 9 ms (9.16%), Space: 18.1 MB (19.08%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -0,0 +1,26 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        # word_list = ["the", "sky", "is", "blue"]
+        # reverse, then join
+        s.strip() # remove leading and trailing white space
+        print(s)
+        word_list = []
+        i = 0
+        while i < len(s):
+            word = ""
+            while i<len(s) and s[i] != " ":
+                word += s[i]
+                i+=1
+            word_list.append(word)
+            while i<len(s) and s[i] == " ":
+                i += 1
+        
+        word_list.reverse() 
+        
+        res = " ".join(word_list)
+        output = res.strip()
+        return output
+            
+            
+            
+        
