52e7c0e6d9723e01481c8095fd8a668eb583e2c8
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-02-28 21:20:29 -0600

    Time: 4 ms (42.14%), Space: 19.2 MB (11.48%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,8 +2,8 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         ddict = {}
         for i,n in enumerate(nums):
-            ddict[n] = i
-        for i,n in enumerate(nums):
-            value = target - n
-            if value in ddict and ddict[value] != i:
-                return [i, ddict[value]]
+            val = target - n
+            if val in ddict:
+                return [i, ddict[val]]
+            else:
+                ddict[n] = i
