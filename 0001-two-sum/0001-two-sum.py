d8e49f7247e509fabf2a1d489f75b5cdd28916ff
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-02-28 21:12:43 -0600

    Time: 3 ms (51.47%), Space: 19.1 MB (24.55%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,20 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # hashmap = defaultdict(int)
-
-        # for i in range(len(nums)):
-        #     key = target - nums[i]
-        #     if key in hashmap and hashmap[key] != i:
-        #         return [i, hashmap[key]]
-        #     hashmap[nums[i]] = i
-        
-
-        # recursive solution O(n) time and space
-        def rec(i, hashmap, target):
-            key = target - nums[i]
-            if key in hashmap and hashmap[key] != i:
-                return [i, hashmap[key]]
-            hashmap[nums[i]] = i
-            return rec(i+1, hashmap, target)
-
-        return rec(0, {}, target)
+        ddict = {}
+        for i,n in enumerate(nums):
+            ddict[n] = i
+        for i,n in enumerate(nums):
+            value = target - n
+            if value in ddict and ddict[value] != i:
+                return [i, ddict[value]]
