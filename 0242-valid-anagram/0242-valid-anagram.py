491f449589df77205531ef13b45b3649436091cc
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-03-03 09:30:57 -0600

    Time: 11 ms (72.9%), Space: 17.9 MB (49.3%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -3,32 +3,13 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        s1 = [0] * 26
-        t1 = [0] * 26
+        count = [0] * 26
 
         for i in range(len(s)):
-            s_idx = ord(s[i]) - ord('a')
-            t_idx = ord(t[i]) - ord('a')
-            s1[s_idx] += 1
-            t1[t_idx] += 1
+            count[ord(s[i])- ord('a')] += 1
+            count[ord(t[i])- ord('a')] -= 1
         
-        if s1 != t1:
-            return False
+        for val in count:
+            if val != 0:
+                return False
         return True
-        
-        # def rec(i, s1, t1):
-        #     if len(s) != len(t):
-        #         return False
-        #     if i == len(s):
-        #         if s1 != t1:
-        #             return False
-        #         else:
-        #             return True
-                
-        #     s_idx = ord(s[i]) - ord('a')
-        #     t_idx = ord(t[i]) - ord('a')
-        #     s1[s_idx] += 1
-        #     t1[t_idx] += 1
-        #     return rec(i+1, s1, t1)
-        
-        # return rec(0, [0]*26, [0]*26)
