049a92d56e8c8788e007142342cac8d774809af0
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-05-23 09:50:38 -0500

    Time: 0 ms (100%), Space: 19.1 MB (10.99%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        ddict = {}
-        for i,n in enumerate(nums):
-            val = target - n
-            if val in ddict:
-                return [i, ddict[val]]
-            else:
-                ddict[n] = i
+        hashmap = {} # num : idx
+        for idx, num in enumerate(nums):
+            key = target - num
+            if key in hashmap and hashmap[key] != idx:
+                return  [hashmap[key], idx]
+            hashmap[num] = idx
+        
