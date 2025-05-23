09b80e2e504eba5624da711812a8b9d414062325
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-05-23 14:23:48 -0500

    Time: 0 ms (100%), Space: 17.9 MB (33.43%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,22 +1,28 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        # n = len(nums)
-        # l, r = 0,0
-        # while r < n:
-        #     if nums[r] != val:
-        #         nums[l] = nums[r]
-        #         l += 1
-        #     r += 1
-        # return l
-
-        def rec(index, left):
-            if (index == len(nums)):
-                return left
-            # nums[i] != val
-            if nums[index] != val:
-                nums[left] = nums[index]
-                return rec(index+1, left+1)
-
-            # nums[i] == val
-            return rec(index+1, left)
-        return rec(0,0)
+        # [3, 2, 2, 3] val = 3
+        # [2,2,-,-]
+        # [2, 2, 3, 3] val = 3
+        # sort it, but that changes my order
+        # find all vals and move to end
+        # iterate reverse and find first non-val element
+        # iterate from left and find val and swap, update right pointer again
+        # left pointer will keep updating until l == r
+        # return k which is the number of elements that aren't val
+        n = len(nums)
+        r = n-1
+        k = 0 # [2233] r = 1, k=2, l = 1
+        for l in range(n):
+            # updates r to be rightmost non-val element
+            while r>0 and nums[r] == val:
+                r-=1
+            if nums[l] != val:
+                k += 1
+            if l < r:
+                # if left pointer is val
+                if nums[l] == val:
+                    nums[l], nums[r] = nums[r], nums[l]
+                    k += 1
+                    r -= 1
+        return k
+            
