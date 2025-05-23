a8d3915dd804130aac85f955a3356270afdfe7ab
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-05-23 13:49:24 -0500

    Time: 9 ms (33.97%), Space: 19.4 MB (51.51%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,25 +1,20 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # candidate, count = nums[0], 0
-
-        # for i in range(len(nums)):
-        #     if count == 0:
-        #         candidate = nums[i]
-        #         count += 1
-        #     elif nums[i] == candidate:
-        #         count += 1
-        #     else:
-        #         count -= 1
-        # return candidate
-        def rec(i, candidate, count):
-            if i == len(nums):
-                return candidate
-            # check if nums[i] == candidate
-            if count == 0:
-                return rec(i+1, nums[i], count+1)
-            elif nums[i] == candidate:
-                return rec(i+1, candidate, count+1)
-            # if not
+       # initial solution : sort the values and then find a count > n/2; O(nlogn + n)
+       # use hashmap to count each number, if count > n//2 return number O(n) time and space
+       # iterate through the array and choose candidate, when count hits 0, change candidate
+       # O(n) time and O(1) space
+        # [3, 2, 3] candidate = 2, count = 1
+        candidate = nums[0]
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] == candidate:
+                count += 1
             else:
-                return rec(i+1, candidate, count-1)
-        return rec(0, nums[0], 0)
+                count -= 1
+            if count == 0:
+                candidate = nums[i]
+                count = 1
+        return candidate
+            
+    
