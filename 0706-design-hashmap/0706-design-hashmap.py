d80839b9081723008bf5cba5e4d1d2aa7b140249
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-05-23 19:15:17 -0500

    Time: 10023 ms (5.01%), Space: 199.9 MB (6.33%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.py
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -4,18 +4,13 @@ class MyHashMap:
     # option 2 - use list with 1000 indexes as there will be 1000 calls. Use node 
 
     def __init__(self):
-        self.map = [[-1, -1] for _ in range(4096)]
+        self.map = [[-1, -1] for _ in range(1000001)]
     
     def get_hash_value(self, key: int) -> int:
-        return key % 4096
+        return key % 1000001
 
     def put(self, key: int, value: int) -> None: # uses hashing and open addressing
         idx = self.get_hash_value(key) # use hash function to get idx
-        # 3 options: address has same key -> update value
-        # address has different key -> go next until empty or same key
-        # address is empty (-1) -> update key and address
-        while self.map[idx][0] != -1 and self.map[idx][0] != key:
-            idx = (idx+1) % 4096
         if self.map[idx][0] == key:
             self.map[idx][1] = value
         else:
@@ -23,20 +18,10 @@ class MyHashMap:
 
     def get(self, key: int) -> int:
         idx = self.get_hash_value(key)
-        orig_idx = idx
-        while self.map[idx][0] != key:
-            idx = (idx+1) % 4096
-            if idx == orig_idx:
-                return -1
         return self.map[idx][1]
 
     def remove(self, key: int) -> None:
         idx = self.get_hash_value(key)
-        orig_idx = idx
-        while self.map[idx][0] != key: # assuming there is a node to delete
-            idx = (idx+1) % 4096
-            if idx == orig_idx:
-                return
         self.map[idx] = [-1, -1]
 
 # Your MyHashMap object will be instantiated and called as such:
