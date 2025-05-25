d138f9e26306f68f73dc4c6ff2f95cc4e3bab1bd
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-05-25 00:09:02 -0500

    Time: 12 ms (18.74%), Space: 17.8 MB (95.26%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,13 +1,19 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        phrase = ""
-        for i in range(len(s)):
-            if s[i].isalnum():
-                phrase += s[i].lower()
-        L,R = 0, len(phrase)-1
-        while L <= R:
-            if phrase[L] != phrase[R]:
+        # return true if after converting all uppercase letters to lowercase and removing all non-alphanumeric characters, reads the same forwards and backwards
+        # create a new list with lowercase alnum characters - O(n) time and space, then check
+        # use 2 pointers to check palindrome or check reverse
+        # conditions: character is uppercase letter, alnum, or not alnum
+        # if not alnum skip, else check lower
+        l, r = 0, len(s)-1 # l = 2, r = 1
+        while l < r:
+            while l < r and  not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if l < r and s[l].lower() != s[r].lower():
                 return False
-            L += 1
-            R -= 1
+            l += 1
+            r -= 1
         return True
+            
