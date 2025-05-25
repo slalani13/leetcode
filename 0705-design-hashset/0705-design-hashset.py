bff04fb2c080fae3c5e848135315e1855878bd6a
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-05-24 22:03:54 -0500

    Time: 32 ms (74.27%), Space: 23.1 MB (51.68%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.py
+++ 0705-design-hashset/0705-design-hashset.py
@@ -1,20 +1,45 @@
+class ListNode:
+
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
 class MyHashSet:
 
     def __init__(self):
-        self.hashset = []
+        self.hashset = [ListNode(-1) for _ in range(1000)]
 
-    def add(self, key: int) -> None:
-        if key not in self.hashset:
-            self.hashset.append(key)
-        
+    def hash(self, key):
+        return key % 1000
+
+    def add(self, key: int) -> None: # O(n/1000)
+        # find the index, check if the key exists, and then add key to linkedlist if it doesn't
+        index = self.hash(key)
+        cur = self.hashset[index]
+        while cur.next:
+            if cur.next.val == key: # if key already exists, return
+                return
+            cur = cur.next
+        cur.next = ListNode(key) # else add key to linkedlist
 
     def remove(self, key: int) -> None:
-        if (self.contains(key)):
-            self.hashset.remove(key)
-
+        # find key in linkedlist under index and remove if exists
+        index = self.hash(key)
+        cur = self.hashset[index]
+        while cur.next:
+            if cur.next.val == key: # if key already exists, return
+                cur.next = cur.next.next
+                return
+            cur = cur.next
+        
     def contains(self, key: int) -> bool:
-        if key in self.hashset:
-            return True
+        # return true or false if key exists
+        index = self.hash(key)
+        cur = self.hashset[index]
+        while cur.next:
+            if cur.next.val == key: # if key already exists, return
+                return True
+            cur = cur.next
         return False
 
 
