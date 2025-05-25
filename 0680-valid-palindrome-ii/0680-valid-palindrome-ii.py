95d8113560077a594fad658828bd8cf0cdea7690
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-05-25 00:32:27 -0500

    Time: 59 ms (70.98%), Space: 19.3 MB (8.87%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,18 +1,15 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         # palindrome can be found with 2 pointers or by reversing the array
-        def isPalindrome(l, r):
-            while l < r:
-                if s[l] != s[r]:
-                    return False
-                l += 1
-                r -= 1
-            return True
-        
-        l, r = 0, len(s) - 1
-        while l < r:
-            if s[l] != s[r]:
-                return isPalindrome(l, r-1) or isPalindrome(l+1, r)
-            l+=1
-            r-=1
+        listS = list(s)
+        l, r = 0, len(listS)-1
+        while l < r: # l = 1, r = 2
+            if listS[l] != listS[r]:
+                includeLeft = listS[l:r]
+                includeRight = listS[l+1:r+1]
+                return includeLeft == includeLeft[::-1] or includeRight == includeRight[::-1]
+            l += 1
+            r -= 1
         return True
+        
+
