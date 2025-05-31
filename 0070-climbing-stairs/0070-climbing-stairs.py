cc863bda467423df49027f25abb569363d7834be
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-05-30 20:15:21 -0500

    Time: 0 ms (100%), Space: 17.7 MB (78.1%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,12 +1,23 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp = [-1] * (n+1)
+        # SRTBOT
+        # Subproblems -> How many steps does it take to reach n-1 steps?
+        # Recursive relation -> F(n) = F(n-2) + F(n-1)
+        # Topology - linear use for loop suffix[i:]
+        # Base case - how many distinct ways can you climb 0 steps? 1 step? 2 steps? 0, 1, 2
+        # Original problem - how many ways can you climb n steps?
+        # Time complexity - O(n)
 
-        # subproblems
-        dp[0] = 1
-        dp[1] = 1
-
-        for i in range(2, len(dp)):
-            dp[i] = dp[i-1] + dp[i-2]
-        
-        return dp[-1]
+        # Template for dp recursive
+        # if in memo, return
+        # base case
+        # recursive relation = memo[i]
+        # return memo[i]
+        def rec(n, memo):
+            if n in memo:
+                return memo[n]
+            if n <= 2:
+                return n
+            memo[n] = rec(n-1, memo) + rec(n-2, memo)
+            return memo[n]
+        return rec(n, {})
