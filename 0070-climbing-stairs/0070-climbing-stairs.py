325e10946a5d9b0961f6d296a7b506493c18c4e5
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-05-30 20:23:00 -0500

    Time: 0 ms (100%), Space: 17.6 MB (95.57%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -13,11 +13,23 @@ class Solution:
         # base case
         # recursive relation = memo[i]
         # return memo[i]
-        def rec(n, memo):
-            if n in memo:
-                return memo[n]
-            if n <= 2:
-                return n
-            memo[n] = rec(n-1, memo) + rec(n-2, memo)
-            return memo[n]
-        return rec(n, {})
+        # def rec(n, memo):
+        #     if n in memo:
+        #         return memo[n]
+        #     if n <= 2:
+        #         return n
+        #     memo[n] = rec(n-1, memo) + rec(n-2, memo)
+        #     return memo[n]
+        # return rec(n, {})
+
+        # convert recursion to dp
+        # 1. recognize the base cases
+        # 2. replace recurrence relation with dp array
+        if n <= 2:
+            return n
+        dp = [0] * (n+1)
+        dp[0], dp[1], dp[2] = 0, 1, 2
+        
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
