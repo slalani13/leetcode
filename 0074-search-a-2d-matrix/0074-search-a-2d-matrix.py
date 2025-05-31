054661512dd5dd539e8efc37a9b0188580ef3c33
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-05-30 22:23:29 -0500

    Time: 0 ms (100%), Space: 18.1 MB (92.91%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,13 +1,35 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        L, R = 0, len(matrix)-1
-
-        while L <= R:
-            mid = L + (R-L) // 2
-            if target in matrix[mid]:
+        # given a sorted matrix and a target, return true if target is in matrix
+        # since we know the search range and it is ordered, we can use binary search
+        # run binary search to find row, then run again to find target within the row
+        # print("I am here")
+        # return False
+        # first binary search
+        target_row = -1
+        left, right = 0, len(matrix)-1
+        # print("I am here")
+        while left <= right:
+            mid = left + (right-left) // 2
+            if matrix[mid][0] <= target and matrix[mid][-1] >= target:
+                target_row = mid
+                break
+            elif matrix[mid][0] > target:
+                right = mid - 1
+            elif matrix[mid][-1] < target:
+                left = mid + 1
+        # print('here')
+        if target_row == -1:
+            return False
+        
+        # second binary search
+        left, right = 0, len(matrix[target_row])
+        while left <= right:
+            mid = left + (right-left) // 2
+            if matrix[target_row][mid] == target:
                 return True
-            elif target < matrix[mid][0]:
-                R = mid - 1
+            elif matrix[target_row][mid] < target:
+                left = mid + 1
             else:
-                L = mid + 1
+                right = mid - 1
         return False
