df821bb3a553838d07427d8ae7b7923949cd7a7b
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-06-01 01:08:58 -0500

    Time: 0 ms (100%), Space: 18.8 MB (69.03%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -6,16 +6,28 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        
-        def dfs(node, summ):
-            if not node:
+        # return true if there is a root to leaf path whose sum is targetSum
+        # dfs to iterate through binary tree
+        # base cases: if not root return false
+        # if node is leaf node check currentSum and return true if equals target sum
+        def helper(root, currentSum):
+            if not root:
                 return False
+            currentSum += root.val
+            if not root.left and not root.right: # leaf node
+                if currentSum == targetSum:
+                    return True
+                return False
+            if helper(root.left, currentSum):
+                return True
+            if helper(root.right, currentSum):
+                return True
+            return False
+        return helper(root, 0)
+        # since int is immutable, each currentSum is a new object
+        # helper(root=5, sum=5) target = 22 -> True
+        # helper(5.left = 4, sum=9) -> True
+        # helper(4.left = 11, sum=20) -> True
+        # helper(11.left = 7, sum=27) -> False
+        # helper(11.right = 2, sum=22) -> True
             
-            summ += node.val
-
-            if not node.left and not node.right:
-                return summ == targetSum
-
-            return dfs(node.right, summ) or dfs(node.left, summ)
-
-        return dfs(root, 0)
