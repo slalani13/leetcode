f4476a103e7334749140d41d0cd30f96c6d2dda2
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-06-01 12:07:21 -0500

    Time: 210 ms (7.86%), Space: 25.4 MB (12.85%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -0,0 +1,27 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        # use dfs when grid[i][j] == 1
+        # dfs calls on each neighbor
+        # if nei is water, per + 1
+
+        ROWS, COLS = len(grid), len(grid[0])
+        visited = set()
+        perimeter = [0]
+
+        def dfs(i, j):
+            # check if inbounds or water, p += 1
+            if i < 0 or i >= ROWS or j < 0 or j >= COLS or grid[i][j] == 0 :
+                perimeter[0] += 1
+                return
+            if (i, j) in visited:
+                return
+            visited.add((i, j))
+            directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+            for dr, dc in directions:
+                dfs(i+dr, j+dc)
+            
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == 1:
+                    dfs(i, j)
+        return perimeter[0]
