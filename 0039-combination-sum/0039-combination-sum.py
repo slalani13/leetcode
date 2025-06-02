a79bb3eb09c251b7cbd684d9af4a018bc3716ecb
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-06-02 02:13:14 -0500

    Time: 9 ms (65.42%), Space: 18 MB (19.41%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,16 +1,29 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        candidates.sort()
-        res, comb = [], []
-        def backtrack(x, idx):
-            if x == 0:
-                res.append(comb[:])
+        # distinct integers candidate array
+        # target interger, return all unique combinations of candidates where the chosen numbers sum to target. Can return in any order.
+        # the same number can be chosen an unlimited number of times
+        # [2, 3, 6, 7] target=7
+        # decision tree - choose each element as a path?
+        # if sum == target, add to list of solutions
+        # each i covers all combinations with nums[i]
+        res, sol = [], []
+        # intuition: with combinations you can only build from current index left to right
+        # to avoid duplicates
+        def backtrack(start, ssum):
+            if ssum == 0:
+                res.append(sol[:])
+            elif ssum < 0:
                 return
-            if x < 0:
-                return
-            for i in range(idx, len(candidates)):
-                comb.append(candidates[i])
-                backtrack(x-candidates[i], i)
-                comb.pop()
-        backtrack(target, 0)
+            for i in range(start, len(candidates)):
+                # add nums[i] to path
+                sol.append(candidates[i])
+                backtrack(i, ssum - candidates[i])
+                # backtrack from path
+                sol.pop()
+
+        backtrack(0, target)
         return res
+
+
+
