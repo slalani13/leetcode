5b29c31445b52684822c6455b7cfc0ff2c1cfc30
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-06-03 13:09:19 -0500

    Time: 6 ms (52.8%), Space: 18.8 MB (98.45%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,27 +1,25 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = defaultdict(list)
-        courses = prerequisites
-        for a,b in courses:
-            graph[b].append(a)
+        # a topological ordering for [0,  1] would be 1 -> 0
+        # create adj list of nodes
+        # find in-degree of each course
+        indegree = [0] * numCourses
+        adj = defaultdict(list)
+        for course, prereq in prerequisites:
+            adj[prereq].append(course)
+            indegree[course] += 1
+        q = deque()
+        for idx, course in enumerate(indegree):
+            if course == 0:
+                q.append(idx)
+        # [0, 0]
+        while q:
+            for _ in range(len(q)):
+                node = q.popleft()
+                for nei in adj[node]:
+                    indegree[nei] -= 1
+                    if indegree[nei] == 0:
+                        q.append(nei)
         
-        UNVISITED, VISITING, VISITED = 0, 1, 2
-        nodes = [UNVISITED] * numCourses
-        def dfs(node):
-            if nodes[node] == VISITING:
-                return False
-            elif nodes[node] == VISITED:
-                return True
-            nodes[node] = VISITING
-            for nei in graph[node]:
-                if not dfs(nei):
-                    return False
-            
-            nodes[node] = VISITED
-            return True
+        return True if sum(indegree) == 0 else False
 
-        for course in range(numCourses):
-            if not dfs(course):
-                return False
-        
-        return True
