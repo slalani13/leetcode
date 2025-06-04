0b582bb647ce180161f61c5fd2b03d3795df6512
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-06-04 12:45:34 -0500

    Time: 4 ms (7.76%), Space: 17.8 MB (64.69%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,17 +1,17 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[-1]*n] * m
-
-        for i in range(n):
-            dp[0][i] = 1
-        
-        for j in range(m):
-            dp[j][0] = 1
-        
-        for i in range(1, m):
-            for j in range(1, n):
-                print(i, j)
-                dp[i][j] = dp[i-1][j] + dp[i][j-1]
-        
-        return dp[m-1][n-1]
-
+        # two inputs m and n, return the number of possible unique paths that a robot cantake to reach the bottom right corner
+        # subproblem - How many unique paths can be reached from one position down and one position to the right
+        # recursive relation - unique(r,c) = unique(r+1, c) + unique(r, c+1)
+        # topological order - iterate through the matrix from m-1, n-1 to 0, 0
+        # base case: row m-1 has only one unique path, col n-1 has only one unique path
+        # original problem - solve for unique(0, 0)
+        # time - O(m*n)
+        def rec(r, c, memo):
+            if r == m-1 or c == n-1:
+                return 1
+            if (r, c) in memo:
+                return memo[(r, c)]
+            memo[(r, c)] = rec(r+1, c, memo) + rec(r, c+1, memo)
+            return memo[(r, c)]
+        return rec(0, 0, {}) 
