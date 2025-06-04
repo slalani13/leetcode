56b13a71930e776331baaef6f2608829dca84129
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-06-04 11:23:55 -0500

    Time: 0 ms (100%), Space: 17.8 MB (34.68%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,17 +1,29 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        dp = [-1] * n
+        # subproblem - What is the max if I choose to rob this house? I can choose to rob this house or the next house. I want the max between my choices
+        # recursive relation: rob(n) = max(nums[i] + rob(i+2), rob(i+1))
+        # topological order - use for loop to iterate through nums
+        # base case: if no houses, max is 0, if one house max is nums[i]
+        # original question: max amount of profit with n houses
+        # time: n subproblems * O(1) = O(n)
 
-        if n == 1:
-            return nums[0]
-        if n == 2:
-            return max(nums[0], nums[1])
-            
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
+        # memo and base case
+        # recursive relastion
+        # return memo[n]
+        n = len(nums)-1
+        def rec(n, memo):
+            if n == -1:
+                return 0
+            if n == 0:
+                return nums[n]
+            if n in memo:
+                return memo[n]
+            memo[n] = max(nums[n] + rec(n-2, memo), rec(n-1, memo))
+            return memo[n]
+        return rec(n, {})
 
-        for i in range(2, n):
-            dp[i] = max(dp[i-1], nums[i] + dp[i-2])
-        
-        return dp[-1]
+        # rec(3, {1: 1, 2: 4}), memo[3] = max(1 + 1, rec(2, memo)))
+        # rec(1, {}), memo[1] = max(1+ 0, 1)))
+        # rec(-1, {}) -> 0
+        # rec(0, {}) -> nums[0]
+        # rec(2, {1:1, }), memo[2] = max(3+1, 1)
