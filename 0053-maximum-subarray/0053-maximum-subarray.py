aa5ac2ad1c9500d31b07e4e56571c8a9acdaaf94
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-06-05 02:18:26 -0500

    Time: 60 ms (60%), Space: 31.7 MB (96.01%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,13 +1,17 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        n = len(nums)
-        if max(nums) < 0:
-            return max(nums)
-        largest = float('-inf')
-        summ = 0
-        for i in range(n):
-            summ += nums[i]
-            if summ < 0:
-                summ = 0
-            largest = max(largest, summ)
-        return largest
+        # using nums subarray return largest sum of a subarray
+        # use Kadane's algorithm, right pointer always moves, left pointer shifts when negative sum
+        max_sum = nums[0]
+        current_sum = 0
+        L = 0
+
+        for R in range(len(nums)):
+            if current_sum < 0:
+                current_sum = 0
+                L = R
+            current_sum += nums[R]
+            max_sum = max(current_sum, max_sum)
+
+        return max_sum
+        # [-1, -5, -4] works
