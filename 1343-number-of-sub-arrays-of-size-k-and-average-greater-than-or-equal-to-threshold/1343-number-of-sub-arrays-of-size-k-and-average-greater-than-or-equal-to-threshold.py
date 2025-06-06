0bf9b3ac1ab5d03d8988fa48c52827b6da10b3d3
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-06-06 11:52:59 -0500

    Time: 54 ms (36.09%), Space: 28.2 MB (39.22%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -1,21 +1,19 @@
 class Solution:
     def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
         # create a window of size k, check the average and update the res
-        if threshold == 0:
-            return len(arr) // k
         numSubarray = 0
         l = 0
         curSum = 0
         for i in range(k):
             curSum += arr[i]
-        if curSum / k >= threshold:
+        if threshold == 0 or curSum / k >= threshold :
             numSubarray += 1
         for r in range(k, len(arr)):
             curSum += arr[r]
             curSum -= arr[l]
             l += 1
             if r-l+1 == k:
-                if curSum / k >= threshold:
+                if threshold == 0 or curSum / k >= threshold:
                     numSubarray += 1
         return numSubarray
         #[2, 2, 2, 2, 5, 5, 5, 8] l=0, r=2, curSum=18, res=0, k -3
