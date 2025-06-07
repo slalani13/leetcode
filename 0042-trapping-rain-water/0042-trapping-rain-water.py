9791abc5857fa9e15f9764d228d8e440e3fe3ed8
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-06-07 09:10:25 -0500

    Time: 35 ms (12.72%), Space: 19.4 MB (39.35%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,20 +1,25 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        # create a max left and right array
+        # array= [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+        # left = [0, 0, 1, 1, 2, 2, 2, 3, 3, 3, 3, 3]
+        # right =[3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 1, 0]
+        # use the left and right walls to determine how much water can be held by checking min wall height
         n = len(height)
+        leftMax, rightMax = 0, 0
         left = [0] * n
         right = [0] * n
-        maxLeft = 0
-        maxRight = 0
-        total = 0
+        i, j = 0, n-1
+        while i <= n and j >= 0:
+            left[i] = leftMax
+            leftMax = max(leftMax, height[i])
+            i += 1
+            right[j] = rightMax
+            rightMax = max(rightMax, height[j])
+            j -= 1
         
+        res = 0
         for i in range(n):
-            j = -i - 1
-            left[i] = maxLeft
-            right[j] = maxRight
-            maxLeft = max(maxLeft, height[i])
-            maxRight = max(maxRight, height[j])
+            res += max(min(left[i], right[i]) - height[i], 0)
+        return res
         
-        for i in range(n):
-            potential = min(left[i], right[i])
-            total += max(0,potential-height[i])
-        return total
