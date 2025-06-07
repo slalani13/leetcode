710e30a3712ad09e6653acf88011701ce6711a16
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-06-06 23:12:42 -0500

    Time: 10 ms (98.22%), Space: 18 MB (39.25%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -3,6 +3,10 @@ class Solution:
         # sliding window of size s1
         # iterate through and update a counts array and check if it matches with the s1Counts
         # continue until len(s2), return false
+        # base checks
+        if len(s1) > len(s2):
+            return False
+        
         s1Counts, counts = [0] * 26, [0] * 26
 
         for i in range(len(s1)):
