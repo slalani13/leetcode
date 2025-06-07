0cc40a0a916cf73c7f8738b93f260bd8458021e7
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-06-06 22:13:53 -0500

    Time: 111 ms (84.57%), Space: 18.1 MB (48.8%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,18 +1,18 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        n = len(s)
-        freq = [0] * 26
+        # return longest substring with at most K replacements
         longest = 0
-        L = 0
-        for R in range(n):
-            idx = ord(s[R]) - ord('A')
-            freq[idx] += 1
-            # while window is not valid
-            while (R-L+1) - max(freq) > k:
-                if L < n-1:
-                    idx = ord(s[L]) - ord('A')
-                    freq[idx] -= 1
-                    L += 1
-            w = R-L+1
-            longest = max(longest, w)
+        counter = defaultdict(int) # key= letter, value= freq
+        l = 0
+        maxfreq = 0
+
+        for r in range(len(s)):
+            counter[s[r]] += 1
+            maxfreq = max(maxfreq, counter[s[r]])
+            while r-l+1 - maxfreq > k:
+                counter[s[l]] -= 1
+                l += 1
+            longest = max(longest, r-l+1)
         return longest
+        # s="ABAB", map={A:2, B:1, }, maxF = 1, w=1, R=1, L=0
+            
