8256540279e2ccb0d6793760e00b3fc7827d09c5
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-06-09 23:54:12 -0500

    Time: 6 ms (64.56%), Space: 19.1 MB (48.14%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,9 +1,33 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        l, r = 0, len(arr)-1
-        while r-l+1 > k:
-            if x-arr[l] <= arr[r]-x:
-                r -= 1
+        # use lower bound binary search to find index <= x
+        # if index is left of array, return index:index+k
+        # if index is right of array, return index-k:index
+        # else, check left and right, if k-left <= right-k: move left pointer, else move right
+        # [1, 2, 3, 4, 5] k=4, x=3
+        #  F  F  T  T  T
+        # [1, 4, 8, 12] x=3
+        #  F  T  T  T
+        def binarySearch():
+            l, r = 0, len(arr)-1
+            while l < r:
+                mid = l+(r-l) // 2
+                if arr[mid] >= x:
+                    r = mid
+                else:
+                    l = mid + 1
+            return l
+        
+        right = binarySearch()
+        left = right-1
+        while right-left-1 < k:
+            if left < 0:
+                return arr[:k]
+            elif right >= len(arr):
+                return arr[-k:]
             else:
-                l += 1
-        return arr[l:r+1]
+                if (x-arr[left]) <= arr[right]-x:
+                    left -= 1
+                else:
+                    right += 1
+        return arr[left+1: right]
