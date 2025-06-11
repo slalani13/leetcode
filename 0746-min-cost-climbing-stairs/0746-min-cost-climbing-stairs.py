2d3290d5efb1349f379b2a0c740ba2a5a23b0b9e
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-06-10 22:05:44 -0500

    Time: 7 ms (21.44%), Space: 18.8 MB (18.92%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,11 +1,27 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
+        # SRTBOT
+        # Subproblem: How much does it cost to reach the i-1 and i-2 step? As getting the min will provide the min-cost before reaching i
+        # Recursive relation: F(i) = min(F(i-1), F(i-2))
+        # Topology: linear, for loop from 0 to i
+        # Base case: steps 0 cost is cost[0] and step 1 cost is cost[1]
+        # Original problem: F(n)
+        # Time: O(n)
         n = len(cost)
-        dp = [-1] * n
-        dp[0] = cost[0]
-        dp[1] = cost[1]
+        cost.append(0)
+        def rec(n, memo):
+            if n == 0:
+                return cost[0]
+            if n == 1:
+                return cost[1]
+            if n in memo:
+                return memo[n]
+            memo[n] = cost[n] + min(rec(n-1, memo), rec(n-2, memo))
+            return memo[n]
+        return rec(n, defaultdict(int))
+        # [10, 15, 30, 0], n=3, memo = {1: 15, 2: 30, 0: 10, 3: 15}
+        # rec(3) = cost[3] + min(30, 15) = 15
+        # rec(2) = 20 + min(15, 10) -> 30
+        # rec(1) -> 15
+        # rec(0) -> 10
 
-        for i in range(2, n):
-            dp[i] = cost[i] + min(dp[i-1], dp[i-2])
-        
-        return min(dp[n-1], dp[n-2])
