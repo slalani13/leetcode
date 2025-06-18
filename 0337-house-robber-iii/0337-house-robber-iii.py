42717efaf689869380b6a224e5baf9a85b993fa2
Shariq Lalani <79872105+slalani13@users.noreply.github.com>
2025-06-18 08:24:20 -0500

    Time: 3 ms (89.5%), Space: 19.5 MB (86.21%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.py
+++ 0337-house-robber-iii/0337-house-robber-iii.py
@@ -6,14 +6,25 @@
 #         self.right = right
 class Solution:
     def rob(self, root: Optional[TreeNode]) -> int:
-        
+        # subproblem: choose to take money from root, or money from left and right
+        # recursive relation: rob(root) = max(rob(not_root), rob(root))
+        # topology - linear iterate over tree
+        # base case: no root: return (0, 0) not robbed amt, robbed amt
+        # original problem: rob(root)
+        # time comp: O(n)
         def dfs(root):
-            # [withRoot, withoutRoot]
             if not root:
-                return [0,0]
+                return (0, 0) # did not rob, robbed
+            
             left = dfs(root.left)
             right = dfs(root.right)
-            withRoot = root.val + left[1] + right[1]
-            withoutRoot = max(left) + max(right)
-            return [withRoot, withoutRoot]
+
+            # choose to rob node
+            rob_node_amount = root.val + left[0] + right[0]
+            # not rob node
+            not_robbed = max(left) + max(right)
+
+            return (not_robbed, rob_node_amount)
+        
         return max(dfs(root))
+        
